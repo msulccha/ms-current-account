d6a9923210da7d7a1b6008e06c7258973b730789
Mauricio Kevin Sulca Chavez <msulccha@emeal.nttdata.com>
2022-08-18 18:58:12 -0500

    primer

---
+++ src/test/java/com/nttdata/mscurrentaccount/MsCurrentAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.mscurrentaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurrentAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
